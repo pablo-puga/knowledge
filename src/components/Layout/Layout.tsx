9d22627d691e9f8d3dc865768e08ab55e5f00844
Pablo Puga <pablo.puga.git@gmail.com>
2022-03-25 17:52:18 +0100

    Updated logo and added it to the navbar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,13 @@
 import type { ReactNode } from 'react';
 import Navbar from '../Navbar';
 
-const NAVBAR_HEIGHT = '40px';
+const NAVBAR_HEIGHT = 40;
 
 const Layout = ({ children }: { children: ReactNode }) => {
     return (
         <>
-            <Navbar style={{ height: NAVBAR_HEIGHT }} />
-            <main className="pt-12" style={{ marginTop: NAVBAR_HEIGHT }}>
+            <Navbar height={NAVBAR_HEIGHT} />
+            <main className="pt-12" style={{ marginTop: `${NAVBAR_HEIGHT}px` }}>
                 {children}
             </main>
         </>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,29 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import type { CSSProperties } from 'react';
-import { FaHome, FaSearch } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 
-const Navbar = ({ style = undefined }: { style?: CSSProperties }) => {
+const Navbar = ({
+    style = undefined,
+    height,
+}: {
+    height: number;
+    style?: CSSProperties;
+}) => {
     return (
         <nav
             className="fixed top-0 left-0 w-full bg-primary shadow-sm shadow-primary flex flex-row items-center justify-between pl-3 pr-1.5"
-            style={{ ...style }}
+            style={{ ...style, height: `${height}px` }}
         >
             <div className="flex flex-row items-center">
                 <Link href={'/'}>
-                    <a className="hover:text-white transition-colors duration-150 text-xl">
-                        <FaHome />
+                    <a className="text-xl flex flex-col items-center hover:drop-shadow-lg transition-shadow duration-200">
+                        <Image
+                            src="/knowledge-256x256.png"
+                            alt="Knowledge Logo"
+                            height={height - 10}
+                            width={height - 10}
+                        />
                     </a>
                 </Link>
                 <h1 className="text-xl font-bold ml-3">
