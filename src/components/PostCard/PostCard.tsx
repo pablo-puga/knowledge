65a68966eaf78fcab8e16109fb07aec8a4e24154
Pablo Puga <pablo.puga.git@gmail.com>
2022-05-31 11:28:32 +0200

    Changed the PostCard title to be a h2

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -24,15 +24,15 @@ const PostCard = ({ title, category, slug, date, tags }: PostCardProps) => {
             style={{ gridTemplateRows: 'auto 1fr' }}
         >
             <Category className="row-span-1">{category}</Category>
-            <h1 className="mt-1 font-semibold text-lg row-span-2">
+            <h2 className="mt-1 font-semibold text-lg row-span-2">
                 <Link href={`/${slug}`}>
                     <a className="text-theme-purple hover:text-theme-green visited:text-theme-indigo visited:hover:text-theme-green transition-colors duration-200">
                         {title}
                     </a>
                 </Link>
-            </h1>
+            </h2>
             {tags && tags.length > 0 && (
-                <ul className="mt-1 flex flex-row gap-2 text-sm font-medium row-span-1">
+                <ul className="mt-3 md:mt-1 flex flex-row gap-2 text-sm font-medium row-span-1">
                     {tags.map((tagName) => (
                         <li key={tagName}>
                             <Tag color={getColorForTag(tagName)}>{tagName}</Tag>
